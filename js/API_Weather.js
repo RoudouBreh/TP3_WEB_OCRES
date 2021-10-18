9e7c852ec499607d130636e1dbaf240bc49fa2fd
Roman PHILIP <romanphlp@gmail.com>
2021-10-18 11:07:32 +0200

    End of TP3 (help by quentin)

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -3,7 +3,7 @@
 // Clé api
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_URL = "https://api.openweathermap.org/data/2.5/forecast/daily";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
@@ -11,7 +11,7 @@ const API_URL_ICON = "http://openweathermap.org/img/wn/";
 class API_WEATHER{
   constructor(city){
     // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
+    if(city === ""){
       city = "paris";
     }
     this.city = city;
@@ -20,9 +20,9 @@ class API_WEATHER{
 
   // Faire la requete à l'API openweathermap
   // Retourne une promise
-  fetchTodayForecast(){
+  getThreeDayForecast(){
     return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+    .get(`${API_URL}?q=${this.city}&units=metric&cnt=4&appid=${API_KEY}`, {
       crossdomain: true
     })
   }
@@ -30,7 +30,4 @@ class API_WEATHER{
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
   }
-
-
-
 }
--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
 
 // Fonction appelée lors du click du bouton
-function start(city) {
+function start() {
+
+  const city = document.getElementById("city-input").value;
+
   // Création de l'objet apiWeather
   const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
-  apiWeather
-    .fetchTodayForecast()
-    .then(function (response) {
+  apiWeather.getThreeDayForecast().then(function (response) {
+
       // Récupère la donnée d'une API
       const data = response.data;
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      for (let i = 0; i < 4; i++) {
 
+        // On peut accéder aux informations qui sont rencoyées dans la réponse et on les stocks dans des constantes
+        // Les informations sont imbriquées dans un json et on utilise les . pour aller chercher dedans ou [?] si c'est
+        // dans un tableau qui est lui meme dans l'objet.
+        const main = data.list[i].weather[0].main;
+        const description = data.list[i].weather[0].description;
+        const temp = data.list[i].temp.day;
+        const icon = apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
 
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-
+        // On modifie le DOM pour y inclure les informations que on a récupéré
+        // ici je profite du fait que j'ai renommé les id avec un numéro en fonction du jour
+        // du coup je fais une boucle qui va aller modif l'element avec l'id "0-forecast-main" puis le suivant
+        // et ainsi de suite t'as capté
+        document.getElementById(i + '-forecast-main').innerHTML = main;
+        document.getElementById(i + '-forecast-more-info').innerHTML = description;
+        document.getElementById(i + '-weather-container').innerHTML = icon;
+        document.getElementById(i + '-forecast-temp').innerHTML = `${temp}°C`;
+      }
     })
     .catch(function (error) {
       // Affiche une erreur
